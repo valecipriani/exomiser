421b43f1f2b4d7acea8fcaf5fedea7857032bc34
Valentina Cipriani <v.cipriani@ucl.ac.uk>
2015-11-10 17:18:07 +0000

    extractVCF_bychr.sh

--- extractVCF_bychr.sh
+++ extractVCF_bychr.sh
@@ -27,7 +27,8 @@ exec >${scriptname}.qsub.out/${scriptname}_${SGE_TASK_ID}_${JOB_ID}.out 2>${scri
 args=( header 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X )
 f=${args[$SGE_TASK_ID]}
 
-uclex_release=mainset_September2015
+uclex_release=mainset_November2015
+
 infile=/scratch2/vyp-scratch2/vincent/GATK/${uclex_release}/${uclex_release}_chr${f}.vcf.gz
 outfolder=/goon2/scratch2/vyp-scratch2/Cipriani_IoO/GATK/data/latestcalls
 
@@ -36,7 +37,7 @@ vcftools=/cluster/project8/vyp/vincent/Software/vcftools_0.1.12a/bin/vcftools
 # sample=IoO_FFS_batch4_LC13186
 # sample=IoO_FFS_batch4_XH13849
 
-for sample in `cat ${outfolder}/NCMDsamples.txt`; do 
+for sample in `cat ${outfolder}/SampleList.txt`; do 
 
 	${vcftools} --gzvcf ${infile} --recode --indv ${sample} --out ${outfolder}/${uclex_release}_${sample}_chr${f} 
 	${vcftools} --vcf ${outfolder}/${uclex_release}_${sample}_chr${f}.recode.vcf --non-ref-ac 1 --recode --out ${outfolder}/${uclex_release}_${sample}_chr${f}_clean
