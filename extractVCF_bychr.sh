63aef449c4743fa99b601ed52ee54556826fb9ed
valecipriani <v.cipriani@ucl.ac.uk>
2015-11-10 16:22:30 +0000

    Update and rename extract_singleVCF_latestcalls_bychrom.sh to extractVCF_bychr.sh

--- extractVCF_bychr.sh
+++ extractVCF_bychr.sh
@@ -21,7 +21,7 @@
 #$ -t 1-23
 set -u
 set -x
-scriptname="extract_singleVCF_latestcalls_bychrom"
+scriptname="extractVCF_bychr"
 mkdir -p ${scriptname}.qsub.out ${scriptname}.qsub.err
 exec >${scriptname}.qsub.out/${scriptname}_${SGE_TASK_ID}_${JOB_ID}.out 2>${scriptname}.qsub.err/${scriptname}_${SGE_TASK_ID}_${JOB_ID}.err
 args=( header 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X )
