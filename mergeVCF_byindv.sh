cbb140784b0c7f03a362d31081a4e06b1e33bf9a
Valentina Cipriani <v.cipriani@ucl.ac.uk>
2015-11-11 14:05:35 +0000

    mergeVCF_byindv.sh

--- mergeVCF_byindv.sh
+++ mergeVCF_byindv.sh
@@ -13,6 +13,8 @@
 # Set the working directory 
 #$ -cwd
 
+#$ -V
+
 #$ -o /dev/null
 #$ -e /dev/null
 
@@ -33,10 +35,10 @@ outfolder=/goon2/scratch2/vyp-scratch2/Cipriani_IoO/GATK/data/latestcalls
 # bgzip=/cluster/project8/vyp/vincent/Software/tabix-0.2.5/bgzip
 # tabix=/cluster/project8/vyp/vincent/Software/tabix-0.2.5/tabix
 
-# sample=IoO_FFS_batch4_LC13186
+sample=IoO_FFS_batch4_LC13186
 # sample=IoO_FFS_batch4_XH13849
 
-for sample in `cat ${outfolder}/SampleList.txt`; do 
+# for sample in `cat ${outfolder}/SampleList.txt`; do 
 
 	for f in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X; do 
 
@@ -45,7 +47,7 @@ for sample in `cat ${outfolder}/SampleList.txt`; do
 		chr${f}=${outfolder}/${uclex_release}_${sample}_chr${f}_clean.recode.vcf.gz
 	done
 
-	vcfconcat ${chr1} ${chr2} ${chr3} ${chr4} ${chr5} ${chr6} ${chr7} ${chr8} ${chr9} ${chr10} ${chr11} ${chr12} ${chr13} ${chr14} ${chr15} ${chr16} ${chr17} ${chr18} ${chr19} ${chr20} ${chr21} ${chr22} ${chrX} | gzip -c > ${outfolder}/${uclex_release}_${sample}.vcf.gz
+	vcf-concat ${chr1} ${chr2} ${chr3} ${chr4} ${chr5} ${chr6} ${chr7} ${chr8} ${chr9} ${chr10} ${chr11} ${chr12} ${chr13} ${chr14} ${chr15} ${chr16} ${chr17} ${chr18} ${chr19} ${chr20} ${chr21} ${chr22} ${chrX} | gzip -c > ${outfolder}/${uclex_release}_${sample}.vcf.gz
 
 	for f in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X; do 
 
@@ -54,4 +56,4 @@ for sample in `cat ${outfolder}/SampleList.txt`; do
  		rm ${outfolder}/${uclex_release}_${sample}_chr${f}_clean.recode.vcf.gz.tbi
 	done 
 
-done
+# done
